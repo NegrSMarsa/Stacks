1fea82cd20d673cf03ceb789318ea5268ad9191d
NegrSMarsa <zoraalikin5@gmail.com>
2024-08-01 11:53:48 +0500

    new ver

--- src/IntStack.java
+++ src/IntStack.java
@@ -53,34 +53,23 @@ class DynStack implements Stack {
 
 public class IntStack {
     public static void main(String[] args){
-        FixedStack mystack1 = new FixedStack(5);
-        FixedStack mystack2 = new FixedStack(8);
-
-        for (int i = 0; i < 5; i++) mystack1.push(i);
-        for (int i = 0; i < 8; i++) mystack2.push(i);
-
-        System.out.println("Стек в mystack1: ");
-        for (int i = 0; i < 5; i++) System.out.println(mystack1.pop());
-        System.out.println();
-
-        System.out.println("Стек в mystack2: ");
-        for (int i = 0; i < 8; i++) System.out.println(mystack2.pop());
-        System.out.println();
-
-        DynStack myDynStack1 = new DynStack(5);
-        DynStack myDynStack2 = new DynStack(8);
-
-        for(int i=0; i<12; i++) myDynStack1 .push(i);
-        for(int i=0; i<20; i++) myDynStack2.push(i);
-
-        System.out.println("Cтeк в myDynStack1:");
+        Stack mystack;
+        //создать переменную ссылки на интерфейс
+        DynStack ds = new DynStack(5);
+        FixedStack fs = new FixedStack(8);
+        mystack = ds; // загрузить динамический стек
+        //разместить числа в стеке
+        for(int i=0; i<12; i++) mystack.push(i);
+        mystack = fs; // загрузить фиксированный стек
+        for(int i=0; i<8; i++) mystack.push(i);
+        mystack = ds;
+        System.out.println("Знaчeния в динамическом стеке:");
         for(int i=0; i<12; i++)
-            System.out.println(myDynStack1.pop());
-        System.out.println();
-
-        System.out.println("Cтeк в myDynStack2:");
-        for(int i=0; i<20; i++)
-            System.out.println(myDynStack2.pop());
-
+            System.out.println(mystack.pop() );
+        mystack = fs;
+        System.out.println("Знaчeния в фиксированном стеке:");
+        for(int i = 0; i < 8; i++){
+            System.out.println(mystack.pop());
+        }
     }
 }
